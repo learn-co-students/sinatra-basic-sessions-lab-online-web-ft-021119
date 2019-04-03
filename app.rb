fec63d49bdf09bdcbc758d088eb2c882c1bc907b
BebopVinh <vinhdo07@gmail.com>
2019-04-03 03:39:57 -0700

    troubleshooting '/checkout'

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   configure do
+      enable :sessions
+      set :session_secret, "secret"
+   end
+
+   get '/' do
+      @session = session
+      binding.pry
+      erb :index
+   end
+
+   post '/checkout' do
+      binding.pry
+      @session[:item] = params[:item]
+   end
 end
