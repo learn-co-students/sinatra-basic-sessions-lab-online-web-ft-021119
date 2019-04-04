9c95ff4407d82494f94e26f9eaaba9d035e38238
ChrstphGrnr <ChristophGruener@me.com>
2019-04-04 09:52:21 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :sessions
+        set :sessions_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    end
+
+    get '/' do 
+        
+        erb :index
+        
+    end
+
+    post '/checkout' do 
+
+        session[params.keys[0]] = params.values[0]
+        @session = session
+
+        # binding.pry
+
+       erb :session_hash
+    
+    
+    end
+
+
+
 end
