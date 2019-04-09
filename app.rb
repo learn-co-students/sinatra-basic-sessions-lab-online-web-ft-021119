5c4f972245c4f40f0bb34c901a286b9a2139080a
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-08 19:41:10 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session["item"] = params[:item]
+        @session = session
+        #key should be item 
+        #value should be the item the user entered to the purchase
+    end
 end
